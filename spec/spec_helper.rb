ce89ad2a728d8c79e66bc270d30b2504597bf819
Gavin Williams <fatmcgav@gmail.com>
2014-01-15 09:59:46 +0000

    (#5) Add rspec testing framework using puppetlabs_spec_helper.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+require 'rubygems'
+require 'puppetlabs_spec_helper/module_spec_helper'
+
+Dir[File.join(File.dirname(__FILE__), 'support', '*.rb')].each do |support_file|
+  require support_file
+end
