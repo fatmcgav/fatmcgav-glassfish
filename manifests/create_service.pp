9daa378b62b2c3c1166a68ea232cb413dceabd5e
Gavin Williams <fatmcgav@gmail.com>
2014-01-15 13:06:56 +0000

    (#7) Change to use domain_name param.

--- manifests/create_service.pp
+++ manifests/create_service.pp
@@ -3,11 +3,11 @@
 # Manages Linux service installation if required
 #
 define glassfish::create_service (
-  $domain  = $name,
-  $runuser = $glassfish::user,
-  $running = false) {
+  $domain_name = $name,
+  $runuser     = $glassfish::user,
+  $running     = false) {
   # Check that we've got a domain name.
-  unless $domain {
+  unless $domain_name {
     fail('Domain name must be specified to install service.')
   }
 
@@ -19,30 +19,30 @@ define glassfish::create_service (
   }
 
   # Create the init file
-  file { "${domain}_servicefile":
+  file { "${domain_name}_servicefile":
     ensure  => present,
-    path    => "/etc/init.d/glassfish_${domain}",
+    path    => "/etc/init.d/glassfish_${domain_name}",
     mode    => '0755',
     content => $service_file,
-    notify  => Service["glassfish_${domain}"]
+    notify  => Service["glassfish_${domain_name}"]
   }
 
   # Need to stop the domain if it was auto-started
   if $running {
-    exec { "stop_${domain}":
-      command => "${glassfish::glassfish_asadmin_path} stop-domain ${domain}",
+    exec { "stop_${domain_name}":
+      command => "${glassfish::glassfish_asadmin_path} stop-domain ${domain_name}",
       user    => $glassfish::user,
-      before  => Service["glassfish_${domain}"]
+      before  => Service["glassfish_${domain_name}"]
     }
   }
 
   # Make sure the service is running and enabled.
-  service { "glassfish_${domain}":
+  service { "glassfish_${domain_name}":
     ensure     => 'running',
     enable     => true,
     hasstatus  => true,
     hasrestart => true,
-    require    => File["${domain}_servicefile"]
+    require    => File["${domain_name}_servicefile"]
   }
 
 }
