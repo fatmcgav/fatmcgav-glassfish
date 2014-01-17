257611ca43a4a09bc0a6493eb8ddc996b36bf6bd
Gavin Williams <fatmcgav@gmail.com>
2014-01-17 10:00:21 +0000

    (#7) Add path_spec.rb class test

---
+++ spec/classes/path_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+# Start to describe glassfish::create_domain class
+describe 'glassfish::path' do
+  
+  context 'on a RedHat OSFamily' do
+    # Set the osfamily fact
+    let(:facts) { {
+      :osfamily => 'RedHat'
+    } }
+
+    # Include required classes
+    let(:pre_condition) { 'include glassfish' }
+    
+    it do 
+      should contain_file('/etc/profile.d/glassfish.sh').with({
+        'ensure'  => 'present',
+        'owner'   => 'root',
+        'group'   => 'root',
+        'mode'    => '0644',
+        'content' =>  /glassfish/
+      }).that_requires('Class[glassfish::install]')
+    end
+  end
+  
+  context 'on a Debian OSFamily' do
+    # Set the osfamily fact
+    let(:facts) { {
+      :osfamily => 'Debian'
+    } }
+    
+    it do 
+      expect { subject }.to raise_error(Puppet::Error, /Debian doesn't support profile.d/)
+    end
+  end
+  
+  context 'on an unsupport OSFamily' do
+    # Set the osfamily fact
+    let(:facts) { {
+      :osfamily => 'Suse'
+    } }
+    
+    it do 
+      expect { subject }.to raise_error(Puppet::Error, /OSFamily Suse is not currently supported./)
+    end
+  end
+  
+end
