7f3ae7ea475ccf97a26f5e96d425883eecd3cbbf
Gavin Williams <fatmcgav@gmail.com>
2014-02-07 11:04:08 +0000

    (#13) Ignore spec files from coverage checks.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,16 @@
 require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
 
-# Coveralls
-require 'simplecov'
-require 'coveralls'
-#Coveralls.wear!
-SimpleCov.formatter = Coveralls::SimpleCov::Formatter
-SimpleCov.start do
-  add_filter 'spec/fixtures/modules/'
+# Coveralls loading 
+begin
+  require 'simplecov'
+  require 'coveralls'
+  SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+  SimpleCov.start do
+    add_filter '/spec/'
+  end
+rescue Exception => e
+  warn "Coveralls disabled"
 end
 
 Dir[File.join(File.dirname(__FILE__), 'support', '*.rb')].each do |support_file|
