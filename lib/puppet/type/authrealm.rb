039d2b39a40f4b2b3e2be3a2b78c62fff5f60984
Gavin Williams <fatmcgav@gmail.com>
2014-02-17 13:09:42 +0000

    (#11) Increase authrealm coverage and add param validation.

--- lib/puppet/type/authrealm.rb
+++ lib/puppet/type/authrealm.rb
@@ -5,29 +5,62 @@ Puppet::Type.newtype(:authrealm) do
   newparam(:name) do
     desc "The realm name."
     isnamevar
+    
+    validate do |name|
+      unless name =~ /^[\w-]+$/
+         raise ArgumentError, "%s is not a valid realm name." % name
+      end
+    end
   end
 
   newparam(:classname) do
-    desc "The Java class name. Ex. com.sun.identity.agents.appserver.v81.AmASRealm"
+    desc "The Java class name. Eg: com.sun.identity.agents.appserver.v81.AmASRealm"
+    validate do |classname|
+      if /^(?:[a-zA-Z_$][a-zA-Z\d_$]*\.)*[A-Z$][a-zA-Z\d_$]{1,}$/.match(classname).nil?
+        raise ArgumentError, "%s is not a valid Java fully qualified type name" % classname
+      end
+    end
   end
 
   newparam(:properties) do
-    desc "The properties. Ex. jaas-context=agentRealm"
+    desc "The properties. Eg: jaas-context=agentRealm"
   end
 
   newparam(:isdefault) do
     desc "Sets realm to default if true."
-    defaultto false
+    defaultto(:false)
+    newvalues(:true, :false)
   end
 
   newparam(:portbase) do
-    desc "The Glassfish domain port base. Default: 4800"
-    defaultto "4800"
+    desc "The Glassfish domain port base. Default: 8000"
+    defaultto '8000'
+
+    validate do |value|
+      raise ArgumentError, "%s is not a valid portbase." % value unless value =~ /^\d{4,5}$/
+    end
+
+    munge do |value|
+      case value
+      when String
+        if value =~ /^[-0-9]+$/
+          value = Integer(value)
+        end
+      end
+
+      return value
+    end
   end
 
   newparam(:asadminuser) do
     desc "The internal Glassfish user asadmin uses. Default: admin"
     defaultto "admin"
+
+    validate do |value|
+      unless value =~ /^[\w-]+$/
+         raise ArgumentError, "%s is not a valid asadmin user name." % value
+      end
+    end
   end
 
   newparam(:passwordfile) do
@@ -47,6 +80,29 @@ Puppet::Type.newtype(:authrealm) do
       unless Puppet.features.root?
         self.fail "Only root can execute commands as other users"
       end
+      unless user =~ /^[\w-]+$/
+         raise ArgumentError, "%s is not a valid user name." % user
+      end
     end
+  end
+  
+  # Validate mandatory params
+  validate do
+    raise Puppet::Error, 'Classname is required.' unless self[:classname]
+  end
+  
+  # Autorequire the user running command
+  autorequire(:user) do
+    self[:user]    
+  end
+  
+  # Autorequire the domain resource, based on portbase
+  autorequire(:domain) do
+    self.catalog.resources.select { |res|
+      next unless res.type == :domain
+      res if res[:portbase] == self[:portbase]
+    }.collect { |res|
+      res[:name]
+    }
   end
 end
--- spec/unit/puppet/type/authrealm_spec.rb
+++ spec/unit/puppet/type/authrealm_spec.rb
@@ -22,4 +22,270 @@ describe Puppet::Type.type(:authrealm) do
     end
   end
   
+  describe "when validating values" do
+    describe "for name" do
+      it "should support an alphanumerical name" do
+        described_class.new(:name => 'realm', :ensure => :present, :classname => 'com.sun.enterprise.security.auth.realm.file.FileRealm')[:name].should == 'realm'
+      end
+
+      it "should support underscores" do
+        described_class.new(:name => 'realm_name', :ensure => :present, :classname => 'com.sun.enterprise.security.auth.realm.file.FileRealm')[:name].should == 'realm_name'
+      end
+   
+      it "should support hyphens" do
+        described_class.new(:name => 'realm-name', :ensure => :present, :classname => 'com.sun.enterprise.security.auth.realm.file.FileRealm')[:name].should == 'realm-name'
+      end
+
+      it "should not support spaces" do
+        expect { described_class.new(:name => 'realm name', :ensure => :present, :classname => 'com.sun.enterprise.security.auth.realm.file.FileRealm') }.to raise_error(Puppet::Error, /realm name is not a valid realm name/)
+      end
+    end
+
+    describe "for ensure" do
+      it "should support present" do
+        described_class.new(:name => 'realm', :ensure => 'present', :classname => 'com.sun.enterprise.security.auth.realm.file.FileRealm')[:ensure].should == :present
+      end
+
+      it "should support absent" do
+        described_class.new(:name => 'realm', :ensure => 'absent', :classname => 'com.sun.enterprise.security.auth.realm.file.FileRealm')[:ensure].should == :absent
+      end
+
+      it "should not support other values" do
+        expect { described_class.new(:name => 'realm', :ensure => 'foo') }.to raise_error(Puppet::Error, /Invalid value "foo"/)
+      end
+
+      it "should not have a default value" do
+        described_class.new(:name => 'realm', :classname => 'com.sun.enterprise.security.auth.realm.file.FileRealm')[:ensure].should == nil
+      end
+    end
+
+    describe "for classname" do 
+      it "should support a valid class name" do
+        described_class.new(:name => 'realm', :classname => 'com.sun.enterprise.security.auth.realm.file.FileRealm')[:classname].should == 'com.sun.enterprise.security.auth.realm.file.FileRealm'
+      end
+      
+      it "should fail an invalid class name" do
+        expect { described_class.new(:name => 'realm', :classname => 'com.sun.enterprise.security.auth.realm.file.') }.to raise_error(Puppet::Error, /is not a valid Java fully qualified type name/)
+      end
+    end
+    
+    describe "for properties" do
+      #TODO: Add properties tests
+    end
+    
+    describe "for isdefault" do
+      it "should support true" do
+        described_class.new(:name => 'realm', :isdefault => 'true', :classname => 'com.sun.enterprise.security.auth.realm.file.FileRealm')[:isdefault].should == :true
+      end
+
+      it "should support false" do
+        described_class.new(:name => 'realm', :isdefault => 'false', :classname => 'com.sun.enterprise.security.auth.realm.file.FileRealm')[:isdefault].should == :false
+      end
+
+      it "should have a default value of false" do
+        described_class.new(:name => 'realm', :classname => 'com.sun.enterprise.security.auth.realm.file.FileRealm')[:isdefault].should == :false
+      end
+
+      it "should not support other values" do
+        expect { described_class.new(:name => 'realm', :isdefault => 'foo') }.to raise_error(Puppet::Error, /Invalid value "foo"/)
+      end
+    end
+    
+    describe "for portbase" do
+      it "should support a numerical value" do
+        described_class.new(:name => 'realm', :portbase => '8000', :ensure => :present, :classname => 'com.sun.enterprise.security.auth.realm.file.FileRealm')[:portbase].should == 8000
+      end
+
+      it "should have a default value of 8000" do
+        described_class.new(:name => 'realm', :ensure => :present, :classname => 'com.sun.enterprise.security.auth.realm.file.FileRealm')[:portbase].should == 8000
+      end
+
+      it "should not support shorter than 4 digits" do
+        expect { described_class.new(:name => 'realm', :portbase => '123', :ensure => :present) }.to raise_error(Puppet::Error, /123 is not a valid portbase./)
+      end
+
+      it "should not support longer than 5 digits" do
+        expect { described_class.new(:name => 'realm', :portbase => '123456', :ensure => :present) }.to raise_error(Puppet::Error, /123456 is not a valid portbase./)
+      end
+
+      it "should not support a non-numeric value" do
+        expect { described_class.new(:name => 'realm', :portbase => 'a', :ensure => :present) }.to raise_error(Puppet::Error, /a is not a valid portbase./)
+      end
+    end
+
+    describe "for asadminuser" do
+      it "should support an alpha name" do
+        described_class.new(:name => 'realm', :asadminuser => 'user', :ensure => :present, :classname => 'com.sun.enterprise.security.auth.realm.file.FileRealm')[:asadminuser].should == 'user'
+      end
+
+      it "should support underscores" do
+        described_class.new(:name => 'realm', :asadminuser => 'admin_user', :ensure => :present, :classname => 'com.sun.enterprise.security.auth.realm.file.FileRealm')[:asadminuser].should == 'admin_user'
+      end
+   
+      it "should support hyphens" do
+        described_class.new(:name => 'realm', :asadminuser => 'admin-user', :ensure => :present, :classname => 'com.sun.enterprise.security.auth.realm.file.FileRealm')[:asadminuser].should == 'admin-user'
+      end
+
+      it "should have a default value of admin" do
+        described_class.new(:name => 'realm', :ensure => :present, :classname => 'com.sun.enterprise.security.auth.realm.file.FileRealm')[:asadminuser].should == 'admin'
+      end
+
+      it "should not support spaces" do
+        expect { described_class.new(:name => 'realm', :asadminuser => 'admin user', :ensure => :present) }.to raise_error(Puppet::Error, /admin user is not a valid asadmin user name/)
+      end
+    end
+
+    describe "for passwordfile" do
+      it "should support a valid file path" do
+        File.expects(:exists?).with('/tmp/asadmin.pass').returns(true).once
+        described_class.new(:name => 'realm', :passwordfile => '/tmp/asadmin.pass', :classname => 'com.sun.enterprise.security.auth.realm.file.FileRealm')[:passwordfile].should == '/tmp/asadmin.pass'
+      end
+
+      it "should fail an invalid file path" do
+        File.expects(:exists?).with('/tmp/nonexistent').returns(false).once
+        expect { described_class.new(:name => 'realm', :passwordfile => '/tmp/nonexistent') }.to raise_error(Puppet::Error, /does not exist/)
+      end
+    end
+
+    describe "for user" do
+      it "should support an alpha name" do
+        Puppet.features.expects(:root?).returns(true).once
+        described_class.new(:name => 'realm', :user => 'glassfish', :ensure => :present, :classname => 'com.sun.enterprise.security.auth.realm.file.FileRealm')[:user].should == 'glassfish'
+      end
+
+      it "should support underscores" do
+        Puppet.features.expects(:root?).returns(true).once
+        described_class.new(:name => 'realm', :user => 'glassfish_user', :ensure => :present, :classname => 'com.sun.enterprise.security.auth.realm.file.FileRealm')[:user].should == 'glassfish_user'
+      end
+   
+      it "should support hyphens" do
+        Puppet.features.expects(:root?).returns(true).once
+        described_class.new(:name => 'realm', :user => 'glassfish-user', :ensure => :present, :classname => 'com.sun.enterprise.security.auth.realm.file.FileRealm')[:user].should == 'glassfish-user'
+      end
+
+      it "should not have a default value of admin" do
+        described_class.new(:name => 'realm', :ensure => :present, :classname => 'com.sun.enterprise.security.auth.realm.file.FileRealm')[:user].should == nil
+      end
+
+      it "should not support spaces" do
+        Puppet.features.expects(:root?).returns(true).once
+        expect { described_class.new(:name => 'realm', :user => 'glassfish user') }.to raise_error(Puppet::Error, /glassfish user is not a valid user name/)
+      end
+      
+      it "should fail if not running as root" do
+        Puppet.features.expects(:root?).returns(false).once
+        expect { described_class.new(:name => 'realm', :user => 'glassfish') }.to raise_error(Puppet::Error, /Only root can execute commands as other users/)
+      end
+    end
+    
+    describe "validate" do
+      it "should not fail with a valid classname" do
+        expect { described_class.new(:name => 'realm', :classname => 'com.sun.enterprise.security.auth.realm.file.FileRealm') }.not_to raise_error
+      end
+      it "should fail with a missing classname" do
+        expect { described_class.new(:name => 'realm') }.to raise_error(Puppet::Error, /Classname is required./)
+      end
+    end
+  end
+  
+  describe "when autorequiring" do    
+    describe "user autorequire" do
+      let :realm do
+        described_class.new(
+          :name      => 'test',
+          :classname => 'com.sun.enterprise.security.auth.realm.file.FileRealm',
+          :user      => 'glassfish'
+        )
+      end
+      
+      # Need to stub user type and provider.
+      let :userprovider do
+        Puppet::Type.type(:user).provide(:fake_user_provider) { mk_resource_methods }
+      end
+      
+      let :user do
+        Puppet::Type.type(:user).new(
+          :name   => 'glassfish',
+          :ensure => 'present'
+        )
+      end
+      
+      let :catalog do
+        Puppet::Resource::Catalog.new
+      end
+  
+      # Stub the user type, and expect File.exists? and Puppet.features.root?
+      before :each do
+        Puppet::Type.type(:user).stubs(:defaultprovider).returns userprovider
+        #File.expects(:exists?).with('/tmp/application.ear').returns(true).once
+        Puppet.features.expects(:root?).returns(true).once
+      end
+      
+      it "should not autorequire a user when no matching user can be found" do
+        catalog.add_resource realm
+        realm.autorequire.should be_empty
+      end
+  
+      it "should autorequire a matching user" do
+        catalog.add_resource realm
+        catalog.add_resource user
+        reqs = realm.autorequire
+        reqs.size.should == 1
+        reqs[0].source.ref.should == user.ref
+        reqs[0].target.ref.should == realm.ref
+      end
+    end
+    
+    describe "domain autorequire" do
+      let :realm do
+        described_class.new(
+          :name      => 'test',
+          :classname => 'com.sun.enterprise.security.auth.realm.file.FileRealm', 
+          :user      => 'glassfish'
+        )
+      end
+      
+      # Need to stub domain type and provider.
+      let :domainprovider do
+        Puppet::Type.type(:domain).provide(:fake_domain_provider) { mk_resource_methods }
+      end
+      
+      let :domain do
+        Puppet::Type.type(:domain).new(
+          :domainname   => 'test',
+          :ensure       => 'present',
+          :passwordfile => '/tmp/password.file',
+          :user         => 'glassfish'
+        )
+      end
+      
+      let :catalog do
+        Puppet::Resource::Catalog.new
+      end
+    
+      # Stub the domain type, and expect File.exists? and Puppet.features.root?
+      before :each do
+        Puppet::Type.type(:domain).stubs(:defaultprovider).returns domainprovider
+        #File.expects(:exists?).with('/tmp/application.ear').returns(true).once
+        Puppet.features.expects(:root?).returns(true).once
+      end
+      
+      it "should not autorequire a domain when no matching domain can be found" do
+        catalog.add_resource realm
+        realm.autorequire.should be_empty
+      end
+    
+      it "should autorequire a matching domain" do
+        # Create catalogue
+        catalog.add_resource realm
+        # Additional expect for domain resource. 
+        Puppet.features.expects(:root?).returns(true).once
+        catalog.add_resource domain
+        reqs = realm.autorequire
+        reqs.size.should == 1
+        reqs[0].source.ref.should == domain.ref
+        reqs[0].target.ref.should == realm.ref
+      end
+    end
+  end
 end
