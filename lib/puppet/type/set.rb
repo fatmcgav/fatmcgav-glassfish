f67dcd69e779b0699865a986681304d3a565463a
Gavin Williams <gavin.williams@weareact.com>
2014-05-27 17:29:09 +0100

    (#11) Update set type unit tests. Also added autorequires for #14.

--- lib/puppet/type/set.rb
+++ lib/puppet/type/set.rb
@@ -6,6 +6,12 @@ Puppet::Type.newtype(:set) do
   newparam(:name) do
     desc "The attribute name."
     isnamevar
+    
+    validate do |value|
+      unless value =~ /^[^\W]?[\w\-\.=]+$/
+         raise ArgumentError, "%s is not a valid set attribute-name." % value
+      end
+    end
   end
 
   newparam(:value) do
@@ -14,12 +20,33 @@ Puppet::Type.newtype(:set) do
   
   newparam(:portbase) do
     desc "The Glassfish domain port base. Default: 4800"
-    defaultto "4800"
+    defaultto '4800'
+    
+    validate do |value|
+      raise ArgumentError, "%s is not a valid portbase." % value unless value =~ /^\d{4,5}$/
+    end
+    
+    munge do |value|
+      case value
+      when String
+        if value =~ /^[-0-9]+$/
+          value = Integer(value)
+        end
+      end
+
+      return value
+    end
   end
 
   newparam(:asadminuser) do
     desc "The internal Glassfish user asadmin uses. Default: admin"
     defaultto "admin"
+    
+    validate do |value|
+      unless value =~ /^[\w-]+$/
+         raise ArgumentError, "%s is not a valid asadmin user name." % value
+      end
+    end
   end
 
   newparam(:passwordfile) do
@@ -35,10 +62,38 @@ Puppet::Type.newtype(:set) do
   newparam(:user) do
     desc "The user to run the command as."
 
-    validate do |user|
+    validate do |value|
       unless Puppet.features.root?
         self.fail "Only root can execute commands as other users"
       end
+      unless value =~ /^[\w-]+$/
+         raise ArgumentError, "%s is not a valid user name." % value
+      end
     end
+  end
+  
+  # Validate mandatory params
+  validate do
+    raise Puppet::Error, 'Value is required.' unless self[:value]
+  end
+  
+  # Autorequire the user running command
+  autorequire(:user) do
+    self[:user]
+  end
+  
+  # Autorequire the password file
+  autorequire(:file) do
+    self[:passwordfile]
+  end
+  
+  # Autorequire the relevant domain
+  autorequire(:domain) do
+    self.catalog.resources.select { |res|
+      next unless res.type == :domain
+      res if res[:portbase] == self[:portbase]
+    }.collect { |res|
+      res[:name]
+    }
   end
 end
