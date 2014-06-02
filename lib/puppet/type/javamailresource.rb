b20286f97e4b4f3231dee0b5504d5b427e6bced1
Gavin Williams <gavin.williams@weareact.com>
2014-06-02 16:29:48 +0100

    Add unit testing to javamailresource type. Increased param validation in the process.

--- lib/puppet/type/javamailresource.rb
+++ lib/puppet/type/javamailresource.rb
@@ -5,11 +5,17 @@ Puppet::Type.newtype(:javamailresource) do
   newparam(:name) do
     desc "The resource name."
     isnamevar
+    
+    validate do |value|
+      unless value =~ /^[^\W]?[\w\-\.\/]+$/
+         raise ArgumentError, "%s is not a valid JavaMail resource name." % value
+      end
+    end
   end
 
   newparam(:mailhost) do
     desc "The mail server address."
-  end  
+  end
 
   newparam(:fromaddress) do
     desc "The mail from address."
@@ -21,12 +27,33 @@ Puppet::Type.newtype(:javamailresource) do
 
   newparam(:portbase) do
     desc "The Glassfish domain port base. Default: 4800"
-    defaultto "4800"
+    defaultto '4800'
+
+    validate do |value|
+      raise ArgumentError, "%s is not a valid portbase." % value unless value =~ /^\d{4,5}$/
+    end
+
+    munge do |value|
+      case value
+      when String
+        if value =~ /^[-0-9]+$/
+          value = Integer(value)
+        end
+      end
+
+      return value
+    end
   end
 
   newparam(:asadminuser) do
     desc "The internal Glassfish user asadmin uses. Default: admin"
     defaultto "admin"
+    
+    validate do |value|
+      unless value =~ /^[\w-]+$/
+         raise ArgumentError, "%s is not a valid asadmin user name." % value
+      end
+    end
   end
 
   newparam(:passwordfile) do
@@ -41,11 +68,29 @@ Puppet::Type.newtype(:javamailresource) do
 
   newparam(:user) do
     desc "The user to run the command as."
-
+    
     validate do |user|
       unless Puppet.features.root?
         self.fail "Only root can execute commands as other users"
       end
+      unless user =~ /^[\w-]+$/
+         raise ArgumentError, "%s is not a valid user name." % user
+      end
     end
+  end
+  
+  # Autorequire the user running command
+  autorequire(:user) do
+    self[:user]    
+  end
+  
+  # Autorequire the domain resource, based on portbase
+  autorequire(:domain) do
+    self.catalog.resources.select { |res|
+      next unless res.type == :domain
+      res if res[:portbase] == self[:portbase]
+    }.collect { |res|
+      res[:name]
+    }
   end
 end 
