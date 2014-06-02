8551807ad9e2da158af7f53166f7e6e6c9852f61
Gavin Williams <gavin.williams@weareact.com>
2014-06-02 16:30:04 +0100

    Fix a deprecation warning.

--- spec/unit/puppet/provider/asadmin_spec.rb
+++ spec/unit/puppet/provider/asadmin_spec.rb
@@ -20,13 +20,13 @@ describe Puppet::Provider::Asadmin do
     
     # Test that correct boolean value is returned
     it "should return true with properties" do
-      subject.hasProperties?('property').should be_true
+      subject.hasProperties?('property').should be_truthy
     end
     it "should return false with nil properties" do
-      subject.hasProperties?(nil).should be_false
+      subject.hasProperties?(nil).should be_falsey
     end
     it "should return false with no properties" do
-      subject.hasProperties?('').should be_false
+      subject.hasProperties?('').should be_falsey
     end
   end
   
