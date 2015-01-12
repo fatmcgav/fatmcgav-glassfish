6e4ffe5382570f891c874e611048b05e20a665d8
Gavin Williams <fatmcgav@gmail.com>
2015-01-12 16:50:41 +0000

    Use Coveralls aswell

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,17 +9,17 @@ rescue Exception => e
   warn "CodeClimate disabled - #{e}"
 end
 
-# # Coveralls loading 
-# begin
-#   require 'simplecov'
-#   require 'coveralls'
-#   SimpleCov.formatter = Coveralls::SimpleCov::Formatter
-#   SimpleCov.start do
-#     add_filter '/spec/'
-#   end
-# rescue Exception => e
-#   warn "Coveralls disabled"
-# end
+# Coveralls loading 
+begin
+  require 'simplecov'
+  require 'coveralls'
+  SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+  SimpleCov.start do
+    add_filter '/spec/'
+  end
+rescue Exception => e
+  warn "Coveralls disabled - #{e}"
+end
 
 Dir[File.join(File.dirname(__FILE__), 'support', '*.rb')].each do |support_file|
   require support_file
