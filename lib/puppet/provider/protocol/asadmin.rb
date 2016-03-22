af4debfbce24034e2257e1bfd07eb06d00e9e198
Richard Gareau <richard.gareau@bell.ca>
2016-03-22 16:16:33 -0400

    PUP-123 Add support for Glassfish protocol definition

---
+++ lib/puppet/provider/protocol/asadmin.rb
@@ -0,0 +1,32 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__),"..","..",".."))
+require 'puppet/provider/asadmin'
+
+Puppet::Type.type(:protocol).provide(:asadmin, :parent => Puppet::Provider::Asadmin) do
+  desc "Glassfish network protocol management."
+
+  def create
+    args = Array.new
+    args << "create-protocol"
+    args << "--securityenabled" << @resource[:transport]
+    args << "--target" << @resource[:target]
+    args << @resource[:name]
+
+    asadmin_exec(args)
+  end
+
+  def destroy
+    args = Array.new
+    args << "delete-protocol" << @resource[:name]
+    args << "--target" << @resource[:target]
+    asadmin_exec(args)
+  end
+
+  def exists?
+    args = [
+      "list-protocol",
+      @resource[:target],
+    ]
+    return true if asadmin_exec(args).include?(@resource[:name])
+    return false
+  end
+end
