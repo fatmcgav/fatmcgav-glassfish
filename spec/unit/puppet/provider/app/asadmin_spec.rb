8bab269463590587edc9d38901218dc0897a7af9
Gavin Williams <fatmcgav@gmail.com>
2016-09-27 16:52:14 +0100

    Rename 'application' type/provider to 'app' to prevent keyword clash in Puppet 4.x

--- spec/unit/puppet/provider/app/asadmin_spec.rb
+++ spec/unit/puppet/provider/app/asadmin_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
-describe Puppet::Type.type(:application).provider(:asadmin) do
+describe Puppet::Type.type(:app).provider(:asadmin) do
   
   before :each do
-    Puppet::Type.type(:application).stubs(:defaultprovider).returns described_class
+    Puppet::Type.type(:app).stubs(:defaultprovider).returns described_class
     Puppet.features.expects(:root?).returns(true).once
   end
   
-  let :application do
-    Puppet::Type.type(:application).new(
+  let :app do
+    Puppet::Type.type(:app).new(
       :name        => 'test',
       :ensure      => :present,
       :user        => 'glassfish',
@@ -27,61 +27,61 @@ describe Puppet::Type.type(:application).provider(:asadmin) do
   
   describe "when asking exists?" do
     it "should return true if resource is present" do
-      application.provider.expects("`").
+      app.provider.expects("`").
         with("su - glassfish -c \"asadmin --port 8048 --user admin list-applications server\"").
         returns("test  <web>  \nCommand list-applications executed successfully. \n")
-      application.provider.should be_exists
+      app.provider.should be_exists
     end
 
     it "should return false if resource is absent" do
-      application.provider.expects("`").
+      app.provider.expects("`").
         with("su - glassfish -c \"asadmin --port 8048 --user admin list-applications server\"").
         returns("Nothing to list \nCommand list-applications executed successfully. \n")
-      application.provider.should_not be_exists
+      app.provider.should_not be_exists
     end
   end
 
   describe "when creating a resource" do
     it "should be able to deploy an application without a context root" do
-      application.provider.expects("`").
+      app.provider.expects("`").
         with("su - glassfish -c \"asadmin --port 8048 --user admin deploy --precompilejsp=true --target server --name test /tmp/test.war\"").
         returns("Application deployed with name test. \nCommand deploy executed successfully. \n")
-      application.provider.create
+      app.provider.create
     end
     
     it "should be able to deploy an application with a context root" do
-      application[:contextroot] = 'test'
-      application.provider.expects("`").
+      app[:contextroot] = 'test'
+      app.provider.expects("`").
         with("su - glassfish -c \"asadmin --port 8048 --user admin deploy --precompilejsp=true --target server --contextroot test --name test /tmp/test.war\"").
         returns("Application deployed with name test. \nCommand deploy executed successfully. \n")
-      application.provider.create
+      app.provider.create
     end
     
     it "should be able to deploy an application with a target" do
-      application[:target] = 'testCluster'
-      application.provider.expects("`").
+      app[:target] = 'testCluster'
+      app.provider.expects("`").
         with("su - glassfish -c \"asadmin --port 8048 --user admin deploy --precompilejsp=true --target testCluster --name test /tmp/test.war\"").
         returns("Application deployed with name test. \nCommand deploy executed successfully. \n")
-      application.provider.create
+      app.provider.create
     end
   end
   
   describe "when destroying a resource" do
     it "should be able to undeploy an application" do
-      application.provider.set(:ensure => :absent)
-      application.provider.expects("`").
+      app.provider.set(:ensure => :absent)
+      app.provider.expects("`").
         with("su - glassfish -c \"asadmin --port 8048 --user admin undeploy --target server test\"").
         returns("Command undeploy executed successfully. \n")
-      application.provider.destroy
+      app.provider.destroy
     end
   end
 
   describe "when refreshing a resource" do
     it "should redeploy an application" do
-      application.provider.expects("`").
+      app.provider.expects("`").
         with("su - glassfish -c \"asadmin --port 8048 --user admin redeploy --name test /tmp/test.war\"").
         returns("Application deployed with name test. \nCommand redeploy executed successfully. \n")
-      application.provider.redeploy
+      app.provider.redeploy
     end
   end
 end
