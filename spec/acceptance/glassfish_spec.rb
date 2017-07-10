7d7a7727954f3c9b2fbfa01b4d6b422796167ff8
Gavin Williams <fatmcgav@gmail.com>
2017-07-10 14:02:53 +0100

    First stab at Travis container builds

--- spec/acceptance/glassfish_spec.rb
+++ spec/acceptance/glassfish_spec.rb
@@ -1,68 +1,86 @@
 require 'spec_helper_acceptance'
 
-context 'with defaults' do
-  let(:manifest) {
-    <<-EOS
-      class { 'glassfish': }
-    EOS
-  }
+describe 'glassfish' do
 
-  it 'should run without errors' do
-    apply_manifest(manifest, :catch_failures => true)
-    expect(apply_manifest(manifest, :catch_failures => true).exit_code).to be_zero
+  case fact('osfamily')
+  when 'RedHat'
+    case fact('operatingsystemmajrelease')
+    when '7'
+      service_file = '/usr/lib/systemd/system/glassfish_test.service'
+    else
+      service_file = '/etc/init.d/glassfish_test'
+    end
   end
 
-  describe user('glassfish') do
-    it { should exist }
-    it { should belong_to_primary_group 'glassfish' }
+  context 'with defaults' do
+    let(:manifest) {
+      <<-EOS
+        class { 'glassfish': }
+      EOS
+    }
+
+    it 'should run without errors' do
+      apply_manifest(manifest, :catch_failures => true)
+      expect(apply_manifest(manifest, :catch_failures => true).exit_code).to be_zero
+    end
+
+    describe user('glassfish') do
+      it { should exist }
+      it { should belong_to_primary_group 'glassfish' }
+    end
+
+    describe file('/usr/local/glassfish-3.1.2.2') do
+      it { should be_directory }
+      it { should be_owned_by 'glassfish' }
+      it { should be_grouped_into 'glassfish' }
+    end
+    
+    describe file('/usr/local/glassfish-3.1.2.2/glassfish/domains/domain1') do
+      it { should_not be_directory }
+    end
   end
 
-  describe file('/usr/local/glassfish-3.1.2.2') do
-    it { should be_directory }
-    it { should be_owned_by 'glassfish' }
-    it { should be_grouped_into 'glassfish' }
-  end
-  
-  describe file('/usr/local/glassfish-3.1.2.2/glassfish/domains/domain1') do
-    it { should_not be_directory }
-  end
-end
+  context 'when creating a domain' do
+    let(:manifest) {
+      <<-EOS
+        class { 'glassfish':
+          create_domain => true,
+          domain_name   => 'test',
+          portbase      => '8000'
+        }
+      EOS
+    }
 
-context 'when creating a domain' do
-  let(:manifest) {
-    <<-EOS
-      class { 'glassfish':
-        create_domain => true,
-        domain_name   => 'test',
-        portbase      => '8000'
-      }
-    EOS
-  }
+    it 'should run without errors' do
+      apply_manifest(manifest, :catch_failures => true, :debug => true)
+      expect(apply_manifest(manifest, :catch_failures => true, :debug => true).exit_code).to be_zero
+    end
 
-  it 'should run without errors' do
-    apply_manifest(manifest, :catch_failures => true)
-    expect(apply_manifest(manifest, :catch_failures => true).exit_code).to be_zero
-  end
+    describe file('/usr/local/glassfish-3.1.2.2/glassfish/domains/test') do
+      it { should be_directory }
+    end
 
-  describe file('/usr/local/glassfish-3.1.2.2/glassfish/domains/test') do
-    it { should be_directory }
-  end
+    describe file(service_file) do
+      it { should be_file }
+    end
 
-  describe service('glassfish_test') do
-    it { should be_enabled }
-    it { should be_running }
-  end
+    describe service('glassfish_test') do
+      it { should be_enabled }
+      it { should be_running }
+    end
 
-  describe process("java") do
-    it { should be_running }
-    its(:count) { should eq 1 }
-  end
+    describe process("java") do
+      it { should be_running }
+      its(:count) { should eq 1 }
+    end
 
-  describe port(8080) do
-    it { should be_listening }
+    describe port(8080) do
+      it { should be_listening }
+    end
+
+    describe port(8048) do
+      it { should be_listening }
+    end
   end
 
-  describe port(8048) do
-    it { should be_listening }
-  end
 end
--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -16,8 +16,12 @@ RSpec.configure do |c|
     # Install module and dependencies
     puppet_module_install(:source => proj_root, :module_name => 'glassfish')
     hosts.each do |host|
-      on host, puppet('module', 'install', 'puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module', 'install', 'puppet-archive'), { :acceptable_exit_codes => [0,1] }
+      on host, puppet('module', 'install', 'puppetlabs-stdlib'), { :acceptable_exit_codes => 0 }
+      on host, puppet('module', 'install', 'puppet-archive'), { :acceptable_exit_codes => 0 }
+      on host, puppet('module', 'install', 'camptocamp-systemd'), { :acceptable_exit_codes => 0 }
+
+      # Copy hiera data into default puppet location
+      copy_hiera_data_to(host, 'spec/fixtures/hiera/hieradata/')
     end
   end
 end
