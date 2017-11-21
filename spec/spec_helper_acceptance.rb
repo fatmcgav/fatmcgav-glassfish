37e77bd3197b163674b4d437ae2f19297c9ab9af
Gavin Williams <fatmcgav@gmail.com>
2017-11-21 09:40:20 +0000

    Add beaker acceptance tests using Docker. Closes #72

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -16,9 +16,41 @@ RSpec.configure do |c|
     # Install module and dependencies
     puppet_module_install(:source => proj_root, :module_name => 'glassfish')
     hosts.each do |host|
-      on host, puppet('module', 'install', 'puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module', 'install', 'puppet-archive'), { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module', 'install', 'camptocamp-systemd'), { :acceptable_exit_codes => [0,1] }
+      on host, puppet('module', 'install', 'puppetlabs-stdlib'), { :acceptable_exit_codes => 0 }
+      on host, puppet('module', 'install', 'puppet-archive'), { :acceptable_exit_codes => 0 }
+      on host, puppet('module', 'install', 'camptocamp-systemd'), { :acceptable_exit_codes => 0 }
+
+      # Copy hiera data into default puppet location
+      copy_hiera_data_to(host, 'spec/fixtures/hiera/hieradata/')
     end
   end
+end
+
+def apply(pp, options = {})
+  if ENV.key?('PUPPET_DEBUG')
+    options[:debug] = true
+  end
+
+  apply_manifest(pp, options)
+end
+
+# Run it twice and test for idempotency
+def apply2(pp)
+  apply(pp, :catch_failures => true)
+  apply(pp, :catch_changes => true)
+end
+
+# probe stolen from:
+# https://github.com/camptocamp/puppet-systemd/blob/master/lib/facter/systemd.rb#L26
+#
+# See these issues for an explination of why this is nessicary rather than
+# using fact() from beaker-facter in the DSL:
+#
+# https://tickets.puppetlabs.com/browse/BKR-1040
+# https://tickets.puppetlabs.com/browse/BKR-1041
+#
+if shell('ps -p 1 -o comm=').stdout =~ /systemd/
+  $systemd = true
+else
+  $systemd = false
 end
